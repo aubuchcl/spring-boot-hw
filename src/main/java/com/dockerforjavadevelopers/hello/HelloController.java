3ed3f0dffd9afd075e0b3582afbb7ff4d41867a3
Chris Aubuchon <aubuchcl@gmail.com>
2023-06-13 19:39:16 -0700

    Update HelloController.java

--- src/main/java/com/dockerforjavadevelopers/hello/HelloController.java
+++ src/main/java/com/dockerforjavadevelopers/hello/HelloController.java
@@ -9,7 +9,7 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Hello World Updated\n";
+        return "Hello World Updated Infiflex\n";
     }
     
 }
